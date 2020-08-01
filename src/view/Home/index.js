51fbb416440f7df2dc69815e421f9a6829cefaaf
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-07-31 21:34:33 -0300

    :art: Filter Area

--- src/view/Home/index.js
+++ src/view/Home/index.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { View } from 'react-native';
 import * as S from './styles';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-const Home = () => {
+const Home = ({actived}) => {
+  const [filter, setFilter] = useState('today');
+
+
   return (
     <S.Container >
       <Header showNotification={true}/>
-      <Footer icon={'add'}/>
+      <S.Filter>
+          <S.FilterBtn onPress={()=> setFilter('all')}>
+              <S.FilterTextActive actived={filter === 'all'}>Todos</S.FilterTextActive>
+          </S.FilterBtn>
+          <S.FilterBtn onPress={()=> setFilter('today')}>
+              <S.FilterTextActive actived={filter === 'today'}>Hoje</S.FilterTextActive>
+          </S.FilterBtn >
+          <S.FilterBtn onPress={()=> setFilter('month')}>
+              <S.FilterTextActive actived={filter === 'month'}>Mês</S.FilterTextActive>
+          </S.FilterBtn>
+          <S.FilterBtn onPress={()=> setFilter('week')}>
+              <S.FilterTextActive actived={filter === 'week'}>Semana</S.FilterTextActive>
+          </S.FilterBtn>
+          <S.FilterBtn onPress={()=> setFilter('year')}>
+              <S.FilterTextActive actived={filter === 'year'}>Ano</S.FilterTextActive>
+          </S.FilterBtn>
+
+        </S.Filter> 
+      <Footer icon={'add'}/>  
     </S.Container>
 
   );
--- src/view/Home/styles.js
+++ src/view/Home/styles.js
@@ -3,8 +3,35 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background: #ffffff;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 export const Title = styled.Text`
   
+`;
+
+export const Filter = styled.View`
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-around;
+  height: 70px;
+  align-items: center;
+ 
+`;
+
+export const FilterBtn = styled.TouchableOpacity``;
+
+export const FilterTextActive = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: ${props => props.actived ?'#FF7811' : '#090909  '};
+  opacity: ${props => props.actived ? 1 : 0.5};
+`;
+
+export const FilterTextInative = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: #090909;
+  opacity: 0.5;
 `;
